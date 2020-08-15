0ddafa5e721e87fa0a23123a55b74497b97c9720
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-08-15 17:38:37 -0300

    Rota que exibe os livros cadastrados

--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Books
+from .serializer import BooksSerializer
 
 
 # Create your views here.
+@api_view(['GET'])
 def index(request):
-    return HttpResponse('Olá, mundo')
+    books = Books.objects.all()
+    serializer = BooksSerializer(books, many=True)
+
+    return Response(serializer.data)
