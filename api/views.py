bdedf12e6d3421e9fbb27deaa2138e8c11759dcc
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-08-15 22:09:00 -0300

    Salvamento de livros no banco

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -8,12 +9,22 @@ from .serializer import AuthorSerializer, BooksSerializer, CategorySerializer
 
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def books(request):
-    books = Books.objects.all()
-    serializer = BooksSerializer(books, many=True)
+    if request.method == 'GET':
+        books = Books.objects.all()
+        serializer = BooksSerializer(books, many=True)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BooksSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors)
 
 
 @api_view(['GET'])
